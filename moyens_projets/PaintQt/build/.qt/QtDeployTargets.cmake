c01a69ba9fbab1de57af9831dd82408f4dd6383a
Laethalf487 <luciletrc@gmail.com>
2025-08-25 16:26:23 +0200

    Rename project folder

--- moyens_projets/PaintQt/build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_PaintQt_FILE C:/Users/Utilisateur/Qt-Projects/moyens_projets/PaintQt/build/PaintQt.exe)
-set(__QT_DEPLOY_TARGET_PaintQt_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_PaintQt_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
